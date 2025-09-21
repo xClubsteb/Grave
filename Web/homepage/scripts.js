71e9dcc67763fccd8816d3de79bdc596a5178b7c
xClubsteb <grebenkinvyacheslav07@gmail.com>
2025-09-21 06:03:32 +0300

    Polished some stuff

--- Web/homepage/scripts.js
+++ Web/homepage/scripts.js
@@ -2,15 +2,30 @@ let link_btn_github = document.getElementById("link-github");
 let link_btn_twitch = document.getElementById("link-twitch");
 let tag_btn = document.getElementById("tag");
 let link_btn_osu = document.getElementById("link-osu");
+let msg = document.getElementById("timer");
+let error_msg = "Something went wrong!";
+let successful = "Copied to clipboard!";
+
+function show_temp_msg(text){
+    const duration = 2000;
+
+    msg.textContent = text;
+
+    setTimeout(() => {
+        msg.textContent = "";
+    }, duration);
+}
 
 link_btn_github.addEventListener('click', () => {
     const text = "https://github.com/xClubsteb";
     navigator.clipboard.writeText(text)
     .then(() => {
         console.log("Copied successfully!");
+        show_temp_msg(successful);
     })
     .catch(err => {
         console.error("Failed to copy: ", err);
+        show_temp_msg(error_msg);
     });
 });
 
@@ -19,9 +34,11 @@ link_btn_twitch.addEventListener('click', () => {
     navigator.clipboard.writeText(text)
     .then(() => {
         console.log("Copied successfully!");
+        show_temp_msg(successful);
     })
     .catch(err => {
         console.error("Failed to copy: ", err);
+        show_temp_msg(error_msg);
     });
 });
 
@@ -31,9 +48,11 @@ tag_btn.addEventListener('click', () => {
     navigator.clipboard.writeText(text)
     .then(() => {
         console.log("Copied successfully!");
+        show_temp_msg(successful);
     })
     .catch(err => {
         console.error("Failed to copy: ", err);
+        show_temp_msg(error_msg);
     });
 });
 
@@ -41,9 +60,12 @@ link_btn_osu.addEventListener('click', () => {
     const text = "https://osu.ppy.sh/users/38296454";
     navigator.clipboard.writeText(text)
     .then(() => {
-    console.log("Copied successfully!");
+        console.log("Copied successfully!");
+        show_temp_msg(successful);
     })
     .catch(err => {
         console.error("Failed to copy: ", err);
+        show_temp_msg(error_msg);
     });
 });
+
